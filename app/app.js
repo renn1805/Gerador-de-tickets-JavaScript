f06186f350a14dd26196b0420e79ebe62e4f2c0d
Renn1805 <renan.alneida.arau@gmail.com>
2025-07-11 14:01:13 -0300

    efeito de preview da foto do avatar consluido

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const inputFile = document.querySelector("#input-file");
+
+inputFile.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.addEventListener("load", function (e) {
+            const readerTarget = e.target;
+            const img = document.createElement("img");
+            img.src = readerTarget.result
+
+            const imgConteiner = document.querySelector('.div-img-upload')
+            imgConteiner.classList.add('conteiner-nova-img')
+            imgConteiner.innerHTML = ""
+            imgConteiner.appendChild(img)
+        });
+
+        reader.readAsDataURL(file);
+    } else {
+        console.log("error");
+    }
+});
