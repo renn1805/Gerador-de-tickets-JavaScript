14f77db11190f42f46f222a07ccfb9d18836fceb
renn1805 <Renan.almeida.arau@gmail.com>
2025-07-12 13:30:33 -0300

    termino das funções da area de upload

--- app/app.js
+++ app/app.js
@@ -1,23 +1,33 @@
 const inputFile = document.querySelector("#input-file");
 
+
+
 inputFile.addEventListener("change", function (e) {
     const inputTarget = e.target;
     const file = inputTarget.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.addEventListener("load", function (e) {
-            const readerTarget = e.target;
-            const img = document.createElement("img");
-            img.src = readerTarget.result
+    if (file.size < 500000) {
+        if (file) {
+            const reader = new FileReader();
+            reader.addEventListener("load", function (e) {
+                const readerTarget = e.target;
+                const img = document.createElement("img");
+                img.src = readerTarget.result
 
-            const imgConteiner = document.querySelector('.div-img-upload')
-            imgConteiner.classList.add('conteiner-nova-img')
-            imgConteiner.innerHTML = ""
-            imgConteiner.appendChild(img)
-        });
+                const imgConteiner = document.querySelector('.div-img-upload')
+                imgConteiner.classList.add('conteiner-nova-img')
+                imgConteiner.innerHTML = ""
+                imgConteiner.appendChild(img)
+            });
 
-        reader.readAsDataURL(file);
+            reader.readAsDataURL(file);
+        } else {
+            console.log("error");
+        }
+
+        const spanUploadInfo = document.querySelector('#upload-info');
+        spanUploadInfo.style.color = "hsl(245, 15%, 58%)";
     } else {
-        console.log("error");
+        alert('imagem muito pesada')
+        spanUploadInfo.style.color = "hsl(7, 71%, 60%)";
     }
 });
